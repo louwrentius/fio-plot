1dc4b2f2a663403e39f94e9e7b9ec036c04fc59d
louwrentius <louwrentius@gmail.com>
2019-11-02 00:51:33 +0100

    first working log trace in graph

--- fio_plot/fiolib/dataimport.py
+++ fio_plot/fiolib/dataimport.py
@@ -2,6 +2,8 @@ import os
 import sys
 import csv
 from pathlib import Path
+import pprint as pprint
+import statistics
 
 def listFioLogFiles(directory):
     absolute_dir = os.path.abspath(directory)
@@ -18,27 +20,57 @@ def listFioLogFiles(directory):
 
     return fiologfiles
 
-def listLogTypeFiles(type, fileList):
-    types = ["clat","lat","iops","bw"]
-    
-    if type in types:
-        searchtype = "_"+type+"."
-        result = [ i for i in fileList if searchtype in i ]
+def returnFilenameFilterString(settings):
+    rw = settings['rw']
+    iodepth = settings['iodepth']
+    numjobs = settings['numjobs']
+    benchtype = settings['type']
+
+    searchstring = str(rw) + "-iodepth-" + str(iodepth) + "-numjobs-" + str(numjobs) + "_" + str(benchtype)
+    return str(searchstring)
+
+def filterLogFiles(settings, fileList):
+    searchstring = returnFilenameFilterString(settings)
+    #pprint.pprint(searchstring)
+    result = [ i for i in fileList if str(searchstring) in i ]
     return result
 
+def mergeSingleDataSet(dataset, datatype, operation):
+    mergedset = []
+    if operation in ['sum', 'mean']:
+        for recordset in dataset:
+            templist = []
+            for item in recordset['data']:
+                templist.append(item[datatype])
+            mergedset.append(templist)
+        if operation == 'sum':
+            merged = [sum(x) for x in zip(*mergedset)]
+        if operation == 'mean':
+            merged = [statistics.mean(x) for x in zip(*mergedset)]
+        return merged 
+
+def mergeDataSet(dataset):
+    timestamps = mergeSingleDataSet(dataset, 'timestamp', 'mean')
+    values = mergeSingleDataSet(dataset, 'value', 'sum')
+    return list(zip(timestamps, values))
+
 def readLogData(inputfile):
+    dataset = []
     if os.path.exists(inputfile):
         with open(inputfile) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            # /1000 -> miliseconds are converted to seconds
-            data = { (int(element[0].strip())/1000): int(element[1].strip())\
-                for element in csv_reader}
-        return data
+            for item in csv_reader:
+                data = {}
+                data['timestamp'] = (int(item[0].strip()))
+                data['value'] = (int(item[1].strip()))
+                dataset.append(data)
+    return dataset
 
-def readLogDataCollection(inputfiles):
+
+def readLogDataFromFiles(settings, inputfiles):
     data = []
     for inputfile in inputfiles:
-        filename = Path(inputfile).resolve().stem.split(".")
+        filename = Path(inputfile).resolve().stem
         logdata = readLogData(inputfile)
         logdict = { "filename": filename, "data": logdata }
         data.append(logdict)
