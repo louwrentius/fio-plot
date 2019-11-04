252e641b3da9571a32efb30aab991c4ce79663be
louwrentius <louwrentius@gmail.com>
2019-11-04 10:51:08 +0100

    backup /broken

--- fio_plot/fiolib/graph2d.py
+++ fio_plot/fiolib/graph2d.py
@@ -1,24 +1,32 @@
-import os
-import sys
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import matplotlib as mpl
 from matplotlib import rcParams, cycler
 import numpy as np
 import pprint as pprint
 
+
 def chart_2d_log_data(config, data):
 
-    #result = [ item['data'] for item in data ]
-    #array = np.array(result)
-    #pprint.pprint(array)
-    #print(array.shape)
+    fig, ax1 = plt.subplots()
+    fig.set_size_inches(10, 6)
+
+    if config['source']:
+        plt.text(1, -0.08, str(config['source']), ha='right', va='top',
+                 transform=ax1.transAxes, fontsize=9)
+
+    plt.suptitle(config['title'] + " | " + config['rw'] + " | iodepth " +
+                 str(config['iodepth']) + " | numjobs " +
+                 str(config['numjobs']) + " | " + str(config['type']),
+                 fontsize=16, horizontalalignment='center')
+
     cmap = plt.cm.tab20c
-    rcParams['axes.prop_cycle'] = cycler(color=cmap(np.linspace(0, 1, len(data))))
-    pprint.pprint(data)
+    rcParams['axes.prop_cycle'] = cycler(
+        color=cmap(np.linspace(0, 1, len(data))))
+    # pprint.pprint(data)
 
     unpacked = list(zip(*data))
-    lines = plt.plot(unpacked[0], unpacked[1])
-    
-    #fig.savefig('test.png')
+    pprint.pprint(data)
+    plt.plot(unpacked[0], unpacked[1])
+    # if config['max_y']:
+
+    # fig.savefig('test.png')
     plt.show()
