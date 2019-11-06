52af1bef3949fff5bf01db5ff2e6438bc52069d3
louwrentius <louwrentius@gmail.com>
2019-11-07 00:58:10 +0100

    added parameter for controlling moving average parameter for graph data'

--- fio_plot/fiolib/graph2d.py
+++ fio_plot/fiolib/graph2d.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams, cycler
 import numpy as np
 import pprint as pprint
+import fiolib.supporting as supporting
 
 
 def chart_2d_log_data(config, data):
@@ -31,27 +32,14 @@ def chart_2d_log_data(config, data):
     axes = {'lat': ax2, 'iops': ax1, 'bw': ax1, 'slat': ax2, 'clat': ax2}
     legend_position = {ax1: 'upper left', ax2: 'upper right'}
 
-    def running_mean(l, N):
-        sum = 0
-        result = list(0 for x in l)
-
-        for i in range(0, N):
-            sum = sum + l[i]
-            result[i] = sum / (i+1)
-
-        for i in range(N, len(l)):
-            sum = sum - l[i-N] + l[i]
-            result[i] = sum / N
-
-        return result
-
     for item in data:
 
         print(item['type'])
         unpacked = list(zip(*item['data']))
         params = []
 
-        running = running_mean(unpacked[1], 4)
+        running = supporting.running_mean(
+            unpacked[1], config['moving_average'])
         maximum = max(unpacked[1]) * 1.25
         axes[item['type']].plot(
             unpacked[0], running, label=f"{item['type']} \
@@ -59,14 +47,6 @@ def chart_2d_log_data(config, data):
             color=colors.pop())
         axes[item['type']].legend(loc=legend_position[axes[item['type']]])
         axes[item['type']].set_ylim([0, maximum])
-        # if 'lat' in item['type']:
-        #    ax2.plot(unpacked[0], unpacked[1],
-        #             label=f"{item['type']} qd: {item['iodepth']} jobs: {item['numjobs']}", color=colors.pop())
-        #    ax2.legend(loc='best')
-        # else:
-        #    ax1.plot(unpacked[0], unpacked[1],
-        #             label=f"{item['type']} qd: {item['iodepth']} jobs: {item['numjobs']}", color=colors.pop())
-        #    ax1.legend(bbox_to_anchor=(0, ))
 
     # if config['max_y']:
 
