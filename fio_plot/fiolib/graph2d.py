936b86a8b6f56e58a05d4934ed92fb4d0c88d4e9
louwrentius <louwrentius@gmail.com>
2019-11-08 01:11:31 +0100

    ton of improvement, but axes labels are still broken

--- fio_plot/fiolib/graph2d.py
+++ fio_plot/fiolib/graph2d.py
@@ -6,48 +6,109 @@ import pprint as pprint
 import fiolib.supporting as supporting
 
 
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.values():
+        sp.set_visible(False)
+
+
 def chart_2d_log_data(config, data):
 
-    fig, ax1 = plt.subplots()
-    fig.set_size_inches(8, 5)
+    fig, host = plt.subplots()
+    fig.subplots_adjust(left=0.22)
 
-    if config['source']:
-        plt.text(1, -0.08, str(config['source']), ha='right', va='top',
-                 transform=ax1.transAxes, fontsize=9)
+    fig.set_size_inches(8, 5)
 
     plt.suptitle(config['title'] + " | " + config['rw'] + " | iodepth " +
                  str(config['iodepth']) + " | numjobs " +
                  str(config['numjobs']) + " | " + str(config['type']),
-                 fontsize=16, horizontalalignment='center')
+                 fontsize=12, horizontalalignment='center')
 
     cmap = plt.cm.jet
     rcParams['axes.prop_cycle'] = cycler(
         color=cmap(np.linspace(0, 1, len(data))))
 
-    # pprint.pprint(data)
-    ax2 = ax1.twinx()
-    colors = ['tab:red', 'tab:green', 'tab:blue',
-              'tab:orange', 'tab:purple', 'r', 'g', 'b', 'c', 'm', 'g', 'b', 'c', 'm']
+    counter = 1
+    axes = {}
+    lines = []
+    labels = []
+    for item in data:
+        if item['type'] not in axes.keys():
+            if counter == 1:
+                axes[item['type']] = host
+            else:
+                axes[item['type']] = host.twinx()
 
-    axes = {'lat': ax2, 'iops': ax1, 'bw': ax1, 'slat': ax2, 'clat': ax2}
-    legend_position = {ax1: 'upper left', ax2: 'upper right'}
+            if counter == 2:
+                axes[item['type']].spines["right"].set_position(
+                    ("axes", -0.28))
+
+        if counter % 3 == 0:
+            make_patch_spines_invisible(axes[item['type']])
+            axes[item['type']].spines["right"].set_visible(True)
+
+        datakey = f"{item['type']}_data"
+        axes[datakey] = list(zip(*item['data']))
+        datalabel = f"{item['type']}_label"
+        axes[datalabel] = (supporting.lookupTable(item['type'])[0])
+
+        #[pprint.pprint(x['type']) for x in data]
+
+        dataplot = f"{item['type']}_plot"
+        unpacked = list(zip(*item['data']))
+        xvalues = unpacked[0]
+        yvalues = unpacked[1]
+        if item['type'] == 'bw':
+            maximum = max(yvalues) * 1.2
+        else:
+            maximum = max(yvalues) * 1.3
+        color = axes[datalabel]['color']
+        axes[dataplot] = axes[item['type']].plot(
+            xvalues, yvalues, label=axes[datalabel]['ylabel'])[0]
+        pprint.pprint(axes[dataplot])
+        axes[item['type']].set_ylim([0, maximum])
+        host.set_xlabel('Time in miliseconds')
+
+        if counter % 3 == 0:
+            axes[item['type']].set_ylabel(
+                axes[datalabel]['ylabel'], rotation='vertical', labelpad=-50)
+        else:
+            axes[item['type']].set_ylabel(axes[datalabel]['ylabel'])
+
+        axes[item['type']].yaxis.label.set_color(axes[dataplot].get_color())
+
+        tkw = dict(size=4, width=1.5)
+        if counter == 0:
+            host.tick_params(axis='x', **tkw)
+        else:
+            axes[item['type']].tick_params(
+                axis='y', colors=axes[dataplot].get_color(), **tkw)
+
+        lines.append(axes[dataplot])
+        labels.append(
+            f"{axes[datalabel]['ylabel']} qd: {item['iodepth']} numjobs: {item['numjobs']}")
+        counter += 1
+        host.legend(lines, labels)
 
     for item in data:
 
-        print(item['type'])
+        print(f"Plotting data for {item['type']}")
         unpacked = list(zip(*item['data']))
-        params = []
-
+        # minimum = min(unpacked[1])
+        # pprint.pprint(minimum)
+        # result = supporting.scaling(minimum)
+        # scaled = [x / result['scale_factor'] for x in unpacked[1]]
         running = supporting.running_mean(
             unpacked[1], config['moving_average'])
-        maximum = max(unpacked[1]) * 1.25
-        axes[item['type']].plot(
-            unpacked[0], running, label=f"{item['type']} \
-                qd: {item['iodepth']} jobs: {item['numjobs']}",
-            color=colors.pop())
-        axes[item['type']].legend(loc=legend_position[axes[item['type']]])
-        axes[item['type']].set_ylim([0, maximum])
-
+        # maximum = max(scaled) * 1.3
+        # axes[item['type']].plot(
+        #    unpacked[0], running, label=f"{item['type']} \
+        #        qd: {item['iodepth']} jobs: {item['numjobs']}")
+        #
+        # axes[item['type']].legend(loc=legend_position[axes[item['type']]])
+        # axes[item['type']].set_ylim([0, maximum])
+        # axes[item['type']].set_ylabel(axes_label[axes[item['type']]])
     # if config['max_y']:
 
     fig.savefig('test.png', dpi=200)
--- fio_plot/fiolib/supporting.py
+++ fio_plot/fiolib/supporting.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin env
+import pprint as pprint
 
 
 def running_mean(l, N):
@@ -14,3 +15,26 @@ def running_mean(l, N):
         result[i] = sum / N
 
     return result
+
+
+def scaling(minimum):
+
+    if minimum < 1:
+        return {'scale_factor': 1.0, 'metric': r'$Latency\ in\ \mu$s'}
+    else:
+        return {'scale_factor': 1000, 'metric': r'$Latency\ in\ ms\ $'}
+
+
+def lookupTable(metric):
+
+    lookup = [{'type': 'iops', 'ylabel': 'IOP/s', 'color': 'tab:blue'},
+              {'type': 'bw',
+                  'ylabel': 'Througput (KB/s)', 'color': 'tab:orange'},
+              {'type': 'lat',
+                  'ylabel': 'LAT Latency (ms)', 'color': 'tab:green'},
+              {'type': 'slat',
+                  'ylabel': 'SLAT Latency (ms)', 'color': 'tab:red'},
+              {'type': 'clat',
+                  'ylabel': 'CLAT Latency (ms)', 'color': 'tab:purple'},
+              ]
+    return [x for x in lookup if x['type'] == metric]
