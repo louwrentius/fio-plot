c3554f2c1b3d54f6f89da2a9f9b07cfbba83d727
louwrentius <louwrentius@gmail.com>
2019-11-12 17:17:39 +0100

    Added title function, added markers

--- fio_plot/fiolib/graph2d.py
+++ fio_plot/fiolib/graph2d.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
+import matplotlib.markers as markers
 from matplotlib.font_manager import FontProperties
 from matplotlib import rcParams, cycler
 import numpy as np
@@ -15,11 +16,26 @@ def make_patch_spines_invisible(ax):
         sp.set_visible(False)
 
 
+def create_title_and_sub(config, plt):
+    plt.suptitle(config['title'])
+    if config['subtitle']:
+        plt.title(config['subtitle'],
+                  fontsize=8, horizontalalignment='center', y=1.02)
+    else:
+        plt.title(config['rw'] + " | iodepth " +
+                  str(config['iodepth']).strip('[]') + " | numjobs " +
+                  str(config['numjobs']).strip('[]') +
+                  " | " + str(config['type']).strip('[]').replace('\'', ''),
+                  fontsize=8, horizontalalignment='center', y=1.02)
+
+
 def chart_2d_log_data(config, data):
 
     datatypes = list(set([x['type'] for x in data]))
 
     fig, host = plt.subplots()
+    fig.set_size_inches(9, 5)
+
     if 'bw' in datatypes:
         fig.subplots_adjust(left=0.21)
         fig.subplots_adjust(bottom=0.22)
@@ -27,12 +43,7 @@ def chart_2d_log_data(config, data):
         fig.subplots_adjust()
         fig.subplots_adjust(bottom=0.22)
 
-    fig.set_size_inches(9, 5)
-
-    plt.suptitle(config['title'] + " | " + config['rw'] + " | iodepth " +
-                 str(config['iodepth']) + " | numjobs " +
-                 str(config['numjobs']) + " | " + str(config['type']),
-                 fontsize=12, horizontalalignment='center')
+    create_title_and_sub(config, plt)
 
     cmap = plt.cm.jet
     rcParams['axes.prop_cycle'] = cycler(
@@ -43,6 +54,11 @@ def chart_2d_log_data(config, data):
     labels = []
     colors = supporting.get_colors()
     counter = 1
+    table_cols = datatypes
+    table_rows = []
+
+    marker_list = list(markers.MarkerStyle.markers.keys())
+    pprint.pprint(marker_list)
 
     for item in data:
 
@@ -64,6 +80,11 @@ def chart_2d_log_data(config, data):
             pprint.pprint([axes[datalabel]])
             yvalues = scaled_data['data']
 
+        if config['enable_markers']:
+            marker_value = marker_list.pop(0)
+        else:
+            marker_value = None
+
         # Determine max / min values to scale graph axis
         if item['type'] == 'bw':
             maximum = max(yvalues) * 1.2
@@ -72,7 +93,9 @@ def chart_2d_log_data(config, data):
 
         # PLOT
         axes[dataplot] = axes[item['type']].plot(
-            xvalues, yvalues, colors.pop(0), label=axes[datalabel]['ylabel'])[0]
+            xvalues, yvalues, marker=marker_value,
+            markevery=(len(yvalues) / (len(yvalues) * 10)),
+            color=colors.pop(0), label=axes[datalabel]['ylabel'])[0]
         # pprint.pprint(axes[dataplot])
         axes[item['type']].set_ylim([0, maximum])
         host.set_xlabel('Time in miliseconds')
@@ -85,6 +108,9 @@ def chart_2d_log_data(config, data):
             rotation=axes[datalabel]['label_rot'],
             labelpad=position)
 
+        # add data to table
+        # table_rows.append([])
+
         # Create legend
         fontP = FontProperties(family='monospace')
         fontP.set_size('xx-small')
@@ -94,8 +120,10 @@ def chart_2d_log_data(config, data):
         counter += 1
         host.legend(lines, labels, prop=fontP,
                     bbox_to_anchor=(0.5, -0.25), loc='lower center', ncol=3)
-    axes[item['type']].grid(ls='dotted')
+
+    # Add grid (or not)
+    if not config['disable_grid']:
+        axes[item['type']].grid(ls='dotted')
+
     now = datetime.now().strftime('%Y-%m-%d_%H%M%S')
     fig.savefig(f"{now}.png", dpi=config['dpi'])
-    # fig.tight_layout()
-    # plt.show()
