e564b678dea411c9a9c8df651243ae851a80ec10
louwrentius <louwrentius@gmail.com>
2019-11-13 16:24:25 +0100

    moved code in separate functions, fixed label positioning

--- fio_plot/fiolib/graph2d.py
+++ fio_plot/fiolib/graph2d.py
@@ -29,102 +29,69 @@ def create_title_and_sub(config, plt):
                   fontsize=8, horizontalalignment='center', y=1.02)
 
 
-def chart_2d_log_data(config, data):
+def chart_2d_log_data(config, dataset):
 
-    datatypes = list(set([x['type'] for x in data]))
+    # datatypes = list(set([x['type'] for x in data]))
+
+    data = supporting.process_dataset(dataset)
+    datatypes = data['datatypes']
 
     fig, host = plt.subplots()
     fig.set_size_inches(9, 5)
 
-    if 'bw' in datatypes:
+    axes = supporting.generate_axes(host, datatypes)
+    create_title_and_sub(config, plt)
+
+    if 'bw' in datatypes and (len(datatypes) > 2):
         fig.subplots_adjust(left=0.21)
         fig.subplots_adjust(bottom=0.22)
     else:
         fig.subplots_adjust()
         fig.subplots_adjust(bottom=0.22)
 
-    create_title_and_sub(config, plt)
-
-    cmap = plt.cm.jet
-    rcParams['axes.prop_cycle'] = cycler(
-        color=cmap(np.linspace(0, 1, len(data))))
-
-    axes = supporting.generate_axes(host, datatypes)
     lines = []
     labels = []
     colors = supporting.get_colors()
-    counter = 1
-    table_cols = datatypes
-    table_rows = []
 
     marker_list = list(markers.MarkerStyle.markers.keys())
 
-    for item in data:
+    fontP = FontProperties(family='monospace')
+    fontP.set_size('xx-small')
 
-        datalabel = f"{item['type']}_label"
-        axes[datalabel] = (supporting.lookupTable(item['type'])[0])
-
-        datakey = f"{item['type']}_data"
-        axes[datakey] = list(zip(*item['data']))
-
-        dataplot = f"{item['type']}_plot"
-        unpacked = list(zip(*item['data']))
-        xvalues = unpacked[0]
-        yvalues = unpacked[1]
-
-        scaled_xaxis = supporting.scale_xaxis_time(xvalues)
-        x_label = scaled_xaxis['format']
-        xvalues = scaled_xaxis['data']
-
-        if 'lat' in item['type']:
-            scaled_data = supporting.scale_yaxis_latency(yvalues)
-            axes[datalabel]['ylabel'] = scaled_data['format']
-            yvalues = scaled_data['data']
+    for item in data['dataset']:
 
         if config['enable_markers']:
             marker_value = marker_list.pop(0)
         else:
             marker_value = None
 
-        # Determine max / min values to scale graph axis
-        if item['type'] == 'bw':
-            maximum = max(yvalues) * 1.2
-        else:
-            maximum = max(yvalues) * 1.3
+        xvalues = item['xvalues']
+        yvalues = item['yvalues']
 
         # PLOT
-        axes[dataplot] = axes[item['type']].plot(
-            xvalues, yvalues, marker=marker_value,
-            markevery=(len(yvalues) / (len(yvalues) * 10)),
-            color=colors.pop(0), label=axes[datalabel]['ylabel'])[0]
+        dataplot = f"{item['type']}_plot"
+        axes[dataplot] = axes[item['type']].plot(xvalues, yvalues, marker=marker_value, markevery=(len(
+            yvalues) / (len(yvalues) * 10)), color=colors.pop(0), label=item['ylabel'])[0]
         # pprint.pprint(axes[dataplot])
-        axes[item['type']].set_ylim([0, maximum])
-        host.set_xlabel(x_label)
+        axes[item['type']].set_ylim([0, item['maximum']])
+        host.set_xlabel(item['xlabel'])
 
         # Label Axis
-        # if counter % 3 == 0:
-        position = supporting.get_label_position(axes[f"{item['type']}_pos"])
+        pprint.pprint(axes[f"{item['type']}_pos"])
+        padding = axes[f"{item['type']}_pos"]
         axes[item['type']].set_ylabel(
-            axes[datalabel]['ylabel'],
-            rotation=axes[datalabel]['label_rot'],
-            labelpad=position)
-
-        # add data to table
-        # table_rows.append([])
+            item['ylabel'],
+            labelpad=padding)
 
         # Create legend
-        fontP = FontProperties(family='monospace')
-        fontP.set_size('xx-small')
         lines.append(axes[dataplot])
+        mean = np.mean(yvalues)
+        stdv = (np.std(yvalues) / mean) * 100
         labels.append(
-            f"{axes[datalabel]['ylabel']} qd: {item['iodepth']:>2} nj: {item['numjobs']:>2} MEAN: {int(round(np.mean(yvalues))):>6} STDV: {round(np.std(yvalues), 2):>6}")
-        counter += 1
-
+            f"{item['ylabel']} qd: {item['iodepth']:>2} nj: {item['numjobs']:>2} MEAN: {int(round(mean)):>6} $\sigma$: {round(stdv, 2):>6}%")
+    pprint.pprint(axes)
     host.legend(lines, labels, prop=fontP,
                 bbox_to_anchor=(0.5, -0.33), loc='lower center', ncol=2)
-    # Add grid (or not)
-    if not config['disable_grid']:
-        axes[item['type']].grid(ls='dotted')
 
     now = datetime.now().strftime('%Y-%m-%d_%H%M%S')
     fig.savefig(f"{now}.png", dpi=config['dpi'])
--- fio_plot/fiolib/supporting.py
+++ fio_plot/fiolib/supporting.py
@@ -67,33 +67,20 @@ def get_colors():
     ]
 
 
-def get_label_position(axis):
-    integer = int(axis[1])
-    if integer % 3 == 0:
-        return -50
-    elif integer % 2 == 0:
-        return 5
-    else:
-        return 0
-
-
 def lookupTable(metric):
 
-    lookup = [
-        {'type': 'iops', 'ylabel': 'IOP/s', 'color': 'tab:blue',
-            'label_pos': 0, 'label_rot': 'vertical'},
-        {'type': 'bw', 'ylabel': 'Througput (KB/s)', 'color': 'tab:orange',
-         'label_pos': -50, 'label_rot': 'vertical'},
-        {'type': 'lat', 'ylabel': 'LAT Latency (ms)', 'color': 'tab:green',
-         'label_pos': 0, 'label_rot': 'vertical'},
-        {'type': 'slat',
-            'ylabel': 'SLAT Latency (ms)',
-            'color': 'tab:red', 'label_pos': 0, 'label_rot': 'vertical'},
-        {'type': 'clat', 'ylabel': 'CLAT Latency (ms)', 'color': 'tab:purple',
-         'label_pos': 0, 'label_rot': 'vertical'},
-
-    ]
-    return [x for x in lookup if x['type'] == metric]
+    lookup = {'iops': {'ylabel': 'IOP/s',
+                       'label_pos': 0, 'label_rot': 'vertical'},
+              'bw': {'ylabel': 'Througput (KB/s)',
+                     'label_pos': -55, 'label_rot': 'vertical'},
+              'lat': {'ylabel': 'LAT Latency (ms)',
+                      'label_pos': 5, 'label_rot': 'vertical'},
+              'slat': {'ylabel': 'SLAT Latency (ms)',
+                       'label_pos': 5, 'label_rot': 'vertical'},
+              'clat': {'ylabel': 'CLAT Latency (ms)',
+                       'label_pos': 5, 'label_rot': 'vertical'},
+              }
+    return lookup[metric]
 
 
 def generate_axes(ax, datatypes):
@@ -102,6 +89,7 @@ def generate_axes(ax, datatypes):
     metrics = ['iops', 'lat', 'bw', 'clat', 'slat']
     tkw = dict(size=4, width=1.5)
     first_not_used = True
+    positions = [0, 5, -55]
 
     for item in metrics:
         if item in datatypes:
@@ -109,11 +97,50 @@ def generate_axes(ax, datatypes):
                 value = ax
                 value.tick_params(axis='x', **tkw)
                 first_not_used = False
+                ax.grid(ls='dotted')
             else:
                 value = ax.twinx()
                 value.tick_params(axis='y', **tkw)
             axes[item] = value
-            axes[f"{item}_pos"] = f"c{(metrics.index(item)) + 1}"
+            axes[f"{item}_pos"] = positions.pop(0)
             if len(axes) == 6:
                 axes[item].spines["right"].set_position(("axes", -0.24))
+                break
     return axes
+
+
+def process_dataset(dataset):
+
+    datatypes = []
+    new_list = []
+    new_structure = {'datatypes': None, 'dataset': None}
+
+    for item in dataset:
+
+        datatypes.append(item['type'])
+
+        unpacked = list(zip(*item['data']))
+        item['xvalues'] = unpacked[0]
+        item['yvalues'] = unpacked[1]
+        item['data'] = None
+        scaled_xaxis = scale_xaxis_time(item['xvalues'])
+        item['xlabel'] = scaled_xaxis['format']
+        item['xvalues'] = scaled_xaxis['data']
+
+        if 'lat' in item['type']:
+            scaled_data = scale_yaxis_latency(item['yvalues'])
+            item['ylabel'] = scaled_data['format']
+            item['yvalues'] = scaled_data['data']
+        else:
+            item['ylabel'] = lookupTable(item['type'])['ylabel']
+
+        if item['type'] == 'bw':
+            item['maximum'] = max(item['yvalues']) * 1.2
+        else:
+            item['maximum'] = max(item['yvalues']) * 1.3
+        new_list.append(item)
+
+    new_structure['datatypes'] = list(set(datatypes))
+    new_structure['dataset'] = new_list
+
+    return new_structure
