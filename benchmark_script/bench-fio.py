81e6d48341983d8278faeabac3b034876c1e97cf
louwrentius <louwrentius@gmail.com>
2020-04-20 04:36:38 +0200

    Benchmark script update

--- benchmark_script/bench-fio.py
+++ benchmark_script/bench-fio.py
@@ -254,4 +254,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f"Control-C pressed - quitting...")
+        sys.exit(1)
