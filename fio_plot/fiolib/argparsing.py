ac4b33da09b7e1d209069dbe48a3bdf8ecf6c86b
louwrentius <louwrentius@gmail.com>
2020-11-29 22:31:30 +0100

    Changes.

---
+++ fio_plot/fiolib/argparsing.py
@@ -0,0 +1,280 @@
+import argparse
+import sys
+
+
+def set_arguments():
+    """Parses all commandline arguments. Based on argparse."""
+    parser = argparse.ArgumentParser(
+        description="Generates charts/graphs from FIO JSON output or logdata."
+    )
+    ag = parser.add_argument_group(title="Generic Settings")
+    ag.add_argument(
+        "-i",
+        "--input-directory",
+        nargs="+",
+        help="input directory where\
+            JSON files or log data (CSV) can be found.",
+        required=True,
+    )
+    ag.add_argument(
+        "-T", "--title", help="specifies title to use in charts", required=True
+    )
+    ag.add_argument("-s", "--source", help="Author")
+
+    exclusive_group = ag.add_mutually_exclusive_group(required=True)
+    exclusive_group.add_argument(
+        "-L",
+        "--iodepth-numjobs-3d",
+        action="store_true",
+        help="\
+            Generates a 3D-chart with iodepth and numjobs on x/y axis and iops or latency on the z-axis.",
+    )
+    exclusive_group.add_argument(
+        "-l",
+        "--latency-iops-2d-qd",
+        action="store_true",
+        help="\
+            Generates a 2D barchart of IOPs and latency for all queue depths given a particular numjobs value.",
+    )
+    exclusive_group.add_argument(
+        "-N",
+        "--latency-iops-2d-nj",
+        action="store_true",
+        help="This graph type is like the \
+        latency-iops-2d-qd barchart but instead of plotting queue depths for a particular numjobs value, it plots \
+            numjobs values for a particular queue depth.",
+    )
+    exclusive_group.add_argument(
+        "-H",
+        "--histogram",
+        action="store_true",
+        help="\
+            Generates a latency histogram for a particular queue depth and numjobs value.",
+    )
+    exclusive_group.add_argument(
+        "-g",
+        "--loggraph",
+        action="store_true",
+        help="This option generates a 2D graph of the log data recorded by FIO.",
+    )
+    exclusive_group.add_argument(
+        "-C",
+        "--compare-graph",
+        action="store_true",
+        help="This option generates a bar chart to compare results from different\
+                                      benchmark runs.",
+    )
+
+    ag.add_argument(
+        "--disable-grid",
+        action="store_true",
+        help="\
+            Disables the dotted grid in the output graph.",
+    )
+    ag.add_argument(
+        "--enable-markers",
+        action="store_true",
+        help="\
+            Enable markers for the plot lines when graphing log data.",
+    )
+    ag.add_argument(
+        "--subtitle",
+        help="\
+            Specify your own subtitle or leave it blank with double quotes.",
+        type=str,
+        default=None,
+    )
+    ag.add_argument(
+        "-d",
+        "--iodepth",
+        type=int,
+        nargs="+",
+        default=None,
+        help="\
+            The I/O queue depth to graph. You can specify multiple values separated by spaces.",
+    )
+    ag.add_argument(
+        "-n",
+        "--numjobs",
+        nargs="+",
+        help="\
+            Specifies for which numjob parameter you want the 2d graphs to be\
+                 generated. You can specify multiple values separated by spaces.",
+        default=None,
+        type=int,
+    )
+    ag.add_argument(
+        "-M",
+        "--maxdepth",
+        nargs="?",
+        default=64,
+        type=int,
+        help="\
+            Maximum queue depth to graph in 3D graph.",
+    )
+    ag.add_argument(
+        "-J",
+        "--maxjobs",
+        help="\
+            Maximum number of jobs to graph in 3D graph.",
+        nargs="?",
+        default=64,
+        type=int,
+    )
+    ag.add_argument(
+        "-D",
+        "--dpi",
+        help="\
+            The chart will be saved with this DPI setting. Higher means larger\
+                     image.",
+        nargs="?",
+        default=200,
+        type=int,
+    )
+    ag.add_argument(
+        "-p",
+        "--percentile",
+        help="\
+            Calculate the percentile, default 99.99th.",
+        nargs="?",
+        default=99.99,
+        type=float,
+    )
+    ag.add_argument(
+        "-r",
+        "--rw",
+        choices=[
+            "read",
+            "write",
+            "randread",
+            "randwrite",
+            "randrw",
+            "trim",
+            "rw",
+            "randtrim",
+            "trimwrite",
+        ],
+        required=True,
+        help="Specifies the kind of data you want to graph.",
+    )
+    ag.add_argument(
+        "-m",
+        "--max",
+        default=None,
+        type=int,
+        help="Optional maximum value for Z-axis in 3D graph.",
+    )
+    ag.add_argument(
+        "-e",
+        "--moving-average",
+        default=None,
+        type=int,
+        help="The moving average helps to smooth out graphs,\
+                         the argument is the size of the moving window\
+                              (default is None to disable). Be carefull as this\
+                                       setting may smooth out issues you may want to be aware of.",
+    )
+    ag.add_argument(
+        "-x",
+        "--min-y",
+        help="Optional minimal value for y-axis. Use 'None' to disable.",
+        type=str,
+        default=0,
+    )
+    ag.add_argument(
+        "-t",
+        "--type",
+        nargs="+",
+        help="\
+            This setting specifies which kind of metric you want to graph.",
+        type=str,
+        choices=["bw", "iops", "lat", "slat", "clat"],
+    )
+    ag.add_argument(
+        "-f",
+        "--filter",
+        nargs="+",
+        help="\
+            filter should be read/write.",
+        type=str,
+        default=("read", "write"),
+        choices=["read", "write"],
+    )
+    ag.add_argument(
+        "--xlabel-depth",
+        help="\
+            Can be used to truncate the most significant folder name from the label. \
+                Often used to strip off folders generated with benchfio (e.g. 4k)",
+        type=int,
+        default=0,
+    )
+    ag.add_argument(
+        "--xlabel-parent",
+        help="\
+            use the parent folder(s) to make the label unique. The number\
+                 represents how many folders up should be included. Default is 1. Use a value of \
+                     0 to remove parent folder name.",
+        type=int,
+        default=1,
+    )
+    ag.add_argument(
+        "--xlabel-segment-size",
+        help="\
+            Truncate folder names to make labels fit the graph. Disabled by default. \
+                The number represents how many characters per \
+                    segment are preserved. Used with -g.",
+        type=int,
+        default=1000,
+    )
+    ag.add_argument(
+        "-w",
+        "--line-width",
+        help="Line width for line graphs. Can be a floating-point value. Used with -g.",
+        type=float,
+        default=1,
+    ),
+    ag.add_argument(
+        "--group-bars",
+        help="When using -l or -C, bars are grouped together by iops/lat type.",
+        action="store_true",
+    )
+    ag.add_argument(
+        "--show-cpu",
+        help="When using the -C or -l option, a table is added with cpu_usr and cpu_sys data.",
+        action="store_true",
+    )
+    ag.add_argument(
+        "--show-ss",
+        help="When using the -C or -l option, a table is added with steadystate data.",
+        action="store_true",
+    )
+    ag.add_argument(
+        "--table-lines",
+        help="Draw the lines within a table (cpu/stdev)",
+        action="store_true",
+    )
+    ag.add_argument(
+        "--max-lat", help="Maximum latency value on y-axis", type=int, default=None
+    )
+    ag.add_argument(
+        "--max-iops", help="Maximum IOPs value on y-axis", type=int, default=None
+    )
+    ag.add_argument(
+        "--max-bw", help="Maximum bandwidth on y-axis", type=int, default=None
+    )
+
+    return parser
+
+
+def get_command_line_arguments(parser):
+    parser = set_arguments()
+    try:
+        args = parser.parse_args()
+    except OSError:
+        parser.print_help()
+        sys.exit(1)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return args
---
+++ fio_plot/fiolib/getdata.py
@@ -0,0 +1,96 @@
+import fiolib.dataimport as logdata
+import fiolib.graph2d as graph
+import fiolib.jsonimport as jsonimport
+import fiolib.bar2d as bar2d
+import fiolib.bar3d as bar3d
+import fiolib.barhistogram as histogram
+
+
+def configure_default_settings(settings, routing_dict, key):
+    if not settings["iodepth"]:
+        settings["iodepth"] = routing_dict[key]["iodepth_default"]
+    if not settings["numjobs"]:
+        settings["numjobs"] = routing_dict[key]["numjobs_default"]
+    settings["query"] = routing_dict[key]["query"]
+    settings["label"] = routing_dict[key]["label"]
+    return settings
+
+
+def get_log_data(settings):
+    if not settings["iodepth"]:
+        settings["iodepth"] = [1]
+    if not settings["numjobs"]:
+        settings["numjobs"] = [1]
+
+    benchmarkfiles = []
+    for input_dir in settings["input_directory"]:
+        benchmarkfiles.extend(logdata.list_fio_log_files(input_dir))
+    logfiles = logdata.filterLogFiles(settings, benchmarkfiles)
+    # pprint.pprint(logfiles)
+    rawdata = logdata.readLogDataFromFiles(settings, logfiles)
+    # pprint.pprint(rawdata)
+    merged = logdata.mergeDataSet(settings, rawdata)
+    return merged
+
+
+def get_json_data(settings):
+    list_of_json_files = jsonimport.list_json_files(settings)
+    # pprint.pprint(list_of_json_files)
+    dataset = jsonimport.import_json_dataset(settings, list_of_json_files)
+    parsed_data = jsonimport.get_flat_json_mapping(settings, dataset)
+    # pprint.pprint(parsed_data)
+    return parsed_data
+
+
+def get_routing_dict():
+    routing_dict = {
+        "loggraph": {
+            "function": graph.chart_2d_log_data,
+            "get_data": get_log_data,
+            "iodepth_default": [1],
+            "numjobs_default": [1],
+            "query": None,
+            "label": None,
+        },
+        "iodepth_numjobs_3d": {
+            "function": bar3d.plot_3d,
+            "get_data": get_json_data,
+            "iodepth_default": [1, 2, 4, 8, 16, 32, 64],
+            "numjobs_default": [1, 2, 4, 8, 16, 32, 64],
+            "query": None,
+            "label": None,
+        },
+        "latency_iops_2d_qd": {
+            "function": bar2d.chart_2dbarchart_jsonlogdata,
+            "get_data": get_json_data,
+            "iodepth_default": [1, 2, 4, 8, 16, 32, 64],
+            "numjobs_default": [1],
+            "query": "iodepth",
+            "label": "Queue depth",
+        },
+        "latency_iops_2d_nj": {
+            "function": bar2d.chart_2dbarchart_jsonlogdata,
+            "get_data": get_json_data,
+            "iodepth_default": [1],
+            "numjobs_default": [1, 2, 4, 8, 16, 32, 64],
+            "query": "numjobs",
+            "label": "Number of jobs",
+        },
+        "histogram": {
+            "function": histogram.chart_latency_histogram,
+            "get_data": get_json_data,
+            "iodepth_default": [1],
+            "numjobs_default": [1],
+            "query": None,
+            "label": None,
+        },
+        "compare_graph": {
+            "function": bar2d.compchart_2dbarchart_jsonlogdata,
+            "get_data": get_json_data,
+            "iodepth_default": [1],
+            "numjobs_default": [1],
+            "query": None,
+            "label": None,
+        },
+    }
+    return routing_dict
