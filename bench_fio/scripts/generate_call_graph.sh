95ff5ddcb8eb2aca61404d6121f8361c34c924af
louwrentius <louwrentius@gmail.com>
2021-11-06 21:45:26 +0100

    test

--- bench_fio/scripts/generate_call_graph.sh
+++ bench_fio/scripts/generate_call_graph.sh
@@ -1,3 +1,3 @@
 # Requires pyan3 to be installled (pip3 install pyan3)
 # The call graph of bench_fio shows that - at this time - the structure needs improvement
-find . -not -path "./bench_fio_test.py" -iname "*.py" | xargs pyan3 --dot --colored --no-defines --grouped | dot -Tpng -Granksep=1.5 > call_graph.png
+find ../ -not -path "./bench_fio_test.py" -iname "*.py" | xargs pyan3 --dot --colored --no-defines --grouped | dot -Tpng -Granksep=1.5 > call_graph.png
