15319dd911bae9740e2a9648c1c2a354c5f77761
louwrentius <louwrentius@gmail.com>
2023-03-06 21:38:52 +0100

    Move some code around

--- fio_plot/fiolib/jsonparsing.py
+++ fio_plot/fiolib/jsonparsing.py
@@ -2,59 +2,6 @@ from . import (
     jsonparsing_support as jsonsupport
 )
 
-def check_for_steadystate(record, mode):
-    keys = record["job options"].keys()
-    if "steadystate" in keys: # remember that we merged global options into job options to make them accessible
-        return True
-    else:
-        return False
-
-
-
-def get_json_mapping(mode, record):
-    """This function contains a hard-coded mapping of FIO nested JSON data
-    to a flat dictionary.
-    """
-    #print(record)
-    #print(record["job options"].keys())
-    dictionary = {
-        "job options": record["job options"],
-        "type": mode,
-        "iodepth": record["job options"]["iodepth"],
-        "numjobs": record["job options"]["numjobs"],
-        "bs": record["job options"]["bs"],
-        "rw": record["job options"]["rw"],
-        "bw": record[mode]["bw"],
-        "iops": record[mode]["iops"],
-        "iops_stddev": record[mode]["iops_stddev"],
-        "lat": record[mode]["lat_ns"]["mean"],
-        "lat_stddev": record[mode]["lat_ns"]["stddev"],
-        "latency_ms": record["latency_ms"],
-        "latency_us": record["latency_us"],
-        "latency_ns": record["latency_ns"],
-        "cpu_usr": record["usr_cpu"],
-        "cpu_sys": record["sys_cpu"]
-
-    }
-
-    # This is hideous, terrible code, I know.
-    if check_for_steadystate(record, mode):
-        dictionary["ss_attained"] = record["steadystate"]["attained"]
-        dictionary["ss_settings"] = record["job options"]["steadystate"] # remember we merged global options into job options
-        dictionary["ss_data_bw_mean"] = record["steadystate"]["data"]["bw_mean"]
-        dictionary["ss_data_iops_mean"] = record["steadystate"]["data"]["iops_mean"]
-
-    else:
-        dictionary["ss_attained"] = None
-        dictionary["ss_settings"] = None
-        dictionary["ss_data_bw_mean"] = None
-        dictionary["ss_data_iops_mean"] = None
-    
-    if jsonsupport.check_for_hostname(record):
-        dictionary["hostname"] = record["hostname"]
-    
-
-    return dictionary
 
 def printkeys(data, depth=0, maxdepth=3):
     """
@@ -69,18 +16,7 @@ def printkeys(data, depth=0, maxdepth=3):
             for item in data:
                 printkeys(item, depth+1)
 
-def get_record_mode(settings): # any of the rw modes must be translated to read or write
-    mapping = {
-        "randrw": settings["filter"][0],
-        "read": "read",
-        "write": "write",
-        "rw": settings["filter"][0],
-        "readwrite": settings["filter"][0],
-        "randread": "read",
-        "randwrite": "write"
-    }
-    mode = mapping[settings["rw"]]
-    return mode
+
 
 def get_json_root_path(record):
     rootpath = None
@@ -92,12 +28,6 @@ def get_json_root_path(record):
     if rootpath is None:
         print("\nNo valid JSON root path found, this should never happen.\n")
     return rootpath
-
-def return_data_row(settings, record):
-    mode = get_record_mode(settings)
-    data = get_json_mapping(mode, record)
-    #pprint.pprint(data)
-    return data
 
 
 def get_json_global_options(record):
@@ -123,39 +53,7 @@ def build_json_mapping(settings, dataset):
         for record in directory["rawdata"]: # each record is the raw JSON data of a file in a directory
             jsonrootpath = get_json_root_path(record)
             globaloptions = get_json_global_options(record)
-            joboptions = None
-            hosts = {}
-            jobs = []
-            just_append = False
-            for job in record[jsonrootpath]:
-                if job["jobname"] != "All clients":
-                    job["job options"] = {**job["job options"], **globaloptions}
-                    if not joboptions:               
-                        joboptions = job["job options"]
-                else:
-                    job["job options"] = joboptions    
-                    job["hostname"] = "All clients"
-                if jsonsupport.check_for_valid_hostname(job):
-                    hostname = job["hostname"]
-                    if hostname not in hosts.keys():
-                        hosts[hostname] = []
-                row = return_data_row(settings, job)  
-                row["fio_version"] = record["fio version"]
-                if hosts:
-                    hosts[hostname].append(row)
-                else:
-                    jobs.append(row)
-            if hosts:
-                for host in hosts.keys():
-                    if len(hosts[host]) > 1 or host == "All clients":
-                        print(host)
-                        directory["data"] = jsonsupport.merge_job_data(settings, hosts)
-                    else:
-                        just_append = True
-            else:
-                just_append = True
-            if just_append:
-                [ directory["data"].append(x) for x in jobs ]
+            jsonsupport.process_json_record(settings, directory, record, jsonrootpath, globaloptions)
 
     directory["data"] = sort_list_of_dictionaries(settings, directory["data"])
     return dataset
