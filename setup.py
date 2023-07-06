51958b09a1329b5997064ab3d9a24a940b985a8d
louwrentius <louwrentius@gmail.com>
2023-07-06 13:36:57 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name="fio-plot",
-        version="1.1.6",
+        version="1.1.7777777",
         author="louwrentius",
         description="Create charts from FIO storage benchmark tool output",
         long_description=long_description,
