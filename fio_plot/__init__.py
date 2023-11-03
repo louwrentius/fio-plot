6841e8e7e3a34ad95628b0aab188a06b6ae5130e
louwrentius <louwrentius@gmail.com>
2023-11-03 14:41:39 +0100

    Cleanup

--- fio_plot/__init__.py
+++ fio_plot/__init__.py
@@ -13,43 +13,6 @@ from .fiolib import (
     defaultsettings
 )
 
-def clean_up_ini_parsing_list_of_strings(value):
-    """
-    When parsing the INI file, a variable like 'colors' is a list of strings.
-    Unfortunately the default return value is a list containing a single empty string.
-    This function is just to clean this up and return None
-    """
-    result = value
-    if isinstance(value, list):
-        if len(value) == 1:
-            if isinstance(value[0], str):
-                if len(value[0]) == 1:
-                    result = None
-    return result
-
-#def merge_ini_and_default(settings, ini):
-#    """
-#    Merging dictionaries like {**settings, **settingsfromini } doesn't seem to result in 
-#    """
-#    returndata = {}
-#    ini_items = ini.items()
-#    for k,v in ini_items:
-#        if not v:
-#            if k in settings.keys():
-#                returndata[k] = settings[k]
-#            else:
-#                returndata[k] = None
-#        else:
-#            returndata[k] = v
-#    for k,v in settings.items():
-#        if k not in returndata:
-#            returndata[k] = v
-#        returndata[k] = clean_up_ini_parsing_list_of_strings(v)
-
-    #print(len(returndata["colors"][0]))
-    #print(returndata)
-    return returndata
-
 def get_settings():
     settings = defaultsettings.get_default_settings()
     parser = None
