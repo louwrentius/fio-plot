d92f942215cb5ff732e9c27343689bdb042a6c07
David Santiago <87186004+davidsntg@users.noreply.github.com>
2024-02-01 13:43:29 +0100

    Update setup.py - missing dependency (#132)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/louwrentius/fio-plot/",
     packages=setuptools.find_packages(),
-    install_requires=["numpy", "matplotlib", "Pillow", "pyan3", "pyparsing"],
+    install_requires=["numpy", "matplotlib", "Pillow", "pyan3", "pyparsing", "rich"],
     include_package_data=True,
     package_data={"bench_fio": ["templates/*.fio", "scripts/*.sh"]},
     entry_points={
